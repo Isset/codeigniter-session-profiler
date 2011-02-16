491eb832cc87fe894a9003268966cbd14cd96ee2
YorickPeterse <info@yorickpeterse.com>
2011-02-16 11:22:25 +0100

    Added a few docblock areas, updated the README and changed the license to the MIT license.

--- README.markdown
+++ README.markdown
@@ -1,25 +1,28 @@
-CodeIgniter-Session-Profiler
-================
+# CodeIgniter-Session-Profiler
 
  * Adds session data to the profiler in CodeIgniter 2.0
  * Adds a table row for each item of session data with the key and value
  * Shows both CI session data and custom session data
 
 Note: I did not write the original implementation, I just adapted it
-for CodeIgniter >= 2.0 . You can find the implementation for 
-CodeIgniter < 2.0 here: 
-http://codeigniter.com/forums/viewthread/60066/
+for CodeIgniter >= 2.0 . You can find the implementation for CodeIgniter < 2.0 [here][http://codeigniter.com/forums/viewthread/60066/]
 
-Requirements
------
-You will need CodeIgniter
+## Requirements
 
-Installation
--------------
+* Codeigniter (Reactor) >= 2.0
+
+### Installation
+
 Drop the libraries/MY_Profiler.php file into your application/libraries folder. 
-If you did not change the MY_ prefix, it should autoload.
+If you did not change the MY_ prefix, it should autoload. If you did change the MY_ prefix
+you'll have to rename both the file and the class to reflect this change.
 
-Contact
------
-If you'd like to request changes, report bug fixes, or contact
-the developer of this library, email <email.n0xie@gmail.com>
+## License
+
+This library is licensed under the MIT license, a copy of this license can be found in
+the file "LICENSE".
+
+## Contact
+
+Bug reports can be submitted to the bugtracker provided by GitHub. If you happen to have
+any questions or just want to say hi feel free to send an Email to github@isset.nl.
--- libraries/MY_Profiler.php
+++ libraries/MY_Profiler.php
@@ -1,57 +1,95 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+
 /**
- * Adds session data to the profiler in CodeIgniter 2.0
- * Adds a table row for each item of session data with the key and value
- * Shows both CI session data and custom session data
+ * # Codeigniter Session Profiler
+ *
+ * * Adds session data to the profiler in CodeIgniter 2.0
+ * * Adds a table row for each item of session data with the key and value
+ * * Shows both CI session data and custom session data
  *
  * Note: I did not write the original implementation, I just adapted it
- * for CodeIgniter >= 2.0 . You can find the implementation for 
- * CodeIgniter < 2.0 here: 
- * http://codeigniter.com/forums/viewthread/60066/
+ * for CodeIgniter >= 2.0 . You can find the implementation for CodeIgniter < 2.0 [here][http://codeigniter.com/forums/viewthread/60066/] 
  *
- * @package		CodeIgniter
- * @subpackage	Libraries
- * @category	Libraries
- * @author		Jeroen v.d. Gulik <http://isset.nl>
- * @license		DBAD License v1.0 <http://philsturgeon.co.uk/code/dbad-license>
- * @version		0.2
+ * @author  Jeroen v.d Gulik, Isset Internet Professionals
+ * @link    http://isset.nl/
+ * @version	0.2
+ * @package Codeigniter Session Profiler
+ * @license MIT License
+ *
+ * Copyright (C) 2010 - 2011, Isset Internet Professionals
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
-
-class MY_Profiler extends CI_Profiler {
-
- 	function __construct($config = array())
+class MY_Profiler extends CI_Profiler 
+{
+	/**
+	 * Constructor method, called whenever the profiler is initialized and can be used
+	 * to set custom configuration options by setting the first argument to an array.
+	 *
+	 * @author Jeroen v.d Gulik
+	 * @access public
+	 * @param  array $config An associative array of custom configuration options.
+	 * @return object
+	 * @see    CI_Profiler::__construct()
+	 */
+ 	public function __construct($config = array())
 	{
 		$this->_available_sections[] = 'session';
+
 		parent::__construct($config);
 	}
 
-    function _compile_session()
+	/**
+	 * Compiles the session data and generates the HTML output.
+	 *
+	 * @author Jeroen v.d Gulik
+	 * @access public
+	 * @return string The HTML containing the session data.
+	 * @see    CI_Profiler::_compile_session()
+	 */
+    public function _compile_session()
 	{
-        $output  = "\n\n";
+        $output  = PHP_EOL . PHP_EOL;
         $output .= '<fieldset style="border:1px solid #009999;padding:6px 10px 10px 10px;margin:20px 0 20px 0;background-color:#eee">';
-        $output .= "\n";
-        $output .= '<legend style="color:#009999;">&nbsp;&nbsp;'.'SESSION DATA'.'&nbsp;&nbsp;</legend>';
-        $output .= "\n";
+        $output .= PHP_EOL;
+        $output .= '<legend style="color:#009999;">&nbsp;&nbsp;' . 'SESSION DATA' . '&nbsp;&nbsp;</legend>';
+        $output .= PHP_EOL;
 
-        if (!is_object($this->CI->session))
+        if ( !is_object($this->CI->session) )
 		{
             $output .= "<div style='color:#009999;font-weight:normal;padding:4px 0 4px 0'>".'No SESSION data exists'."</div>";
         } 
 		else
 		{
-		    $output .= "\n\n<table cellpadding='4' cellspacing='1' border='0' width='100%'>\n";
-            $sess = get_object_vars($this->CI->session);
+		    $output .= PHP_EOL . PHP_EOL . "<table cellpadding='4' cellspacing='1' border='0' width='100%'>" . PHP_EOL;
+            $sess    = get_object_vars($this->CI->session);
 
-            foreach ($sess['userdata'] as $key => $val)
+            foreach ( $sess['userdata'] as $key => $val )
 			{
-                if ( ! is_numeric($key))
+                if ( !is_numeric($key) )
 				{
-                    $key = "'".$key."'";
+                    $key = "'" . $key . "'";
                 }
 
-                $output .= "<tr><td width='50%' style='color:#000;background-color:#ddd;'>&#36;_SESSION[".$key."]&nbsp;&nbsp; </td><td width='50%' style='color:#009999;font-weight:normal;background-color:#ddd;'>";
+                $output .= "<tr><td width='50%' style='color:#000;background-color:#ddd;'>&#36;_SESSION[" . $key . "]&nbsp;&nbsp; </td><td width='50%' style='color:#009999;font-weight:normal;background-color:#ddd;'>";
 
-                if (is_array($val))
+                if ( is_array($val) )
 				{
                     $output .= "<pre>" . htmlspecialchars(stripslashes(print_r($val, true))) . "</pre>";
                 } 
@@ -60,10 +98,10 @@ class MY_Profiler extends CI_Profiler {
                     $output .= htmlspecialchars(stripslashes($val));
                 }
 
-                $output .= "</td></tr>\n";
+                $output .= "</td></tr>" . PHP_EOL;
             }
 
-			$output .= "</table>\n";
+			$output .= "</table>" . PHP_EOL;
         }
 
         $output .= "</fieldset>";
